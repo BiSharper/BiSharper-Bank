9de61eabf23c2944d1e044215c403fcf640a7f81
ryann <flipperplz@protonmail.com>
2024-03-07 17:38:59 -0800

    feat: Upgrade To RFSA-0.2.0

--- src/archive.rs
+++ src/archive.rs
@@ -1,79 +1,55 @@
 use std::collections::HashMap;
-use rfsa::{GFS_SEPARATOR, PathLike, ReadableVFile, ReadableVMetadata, VDirectory, VFile, VFileContainer, VFileSystem, VPath, VPathIterator, WritableVFile, WritableVMetadata};
-use rfsa::error::{VFSError, VFSResult};
+use std::ops::{Deref, DerefMut};
+use rfsa::{ReadableVFile, ReadableVMetadata, VDirectory, VFile, VFileSystem, VPath, WritableVFile, WritableVMetadata};
+use rfsa::impls::memory::MemoryFileSystem;
+
 use crate::BankFileMeta;
 
 pub type BankFile = VFile<BankFileMeta>;
-pub type ReadableBankFile<'a> = ReadableVFile<'a, BankFileMeta>;
-pub type WritableBankFile<'a> = WritableVFile<'a, BankFileMeta>;
-pub type ReadableBankMetadata<'a> = ReadableVMetadata<'a, BankFileMeta>;
-pub type WritableBankMetadata<'a> = WritableVMetadata<'a, BankFileMeta>;
+pub type ReadableBankFile = ReadableVFile<BankFileMeta>;
+pub type WritableBankFile<'a> = WritableVFile<'a, BankFileMeta, BankArchive>;
+pub type ReadableBankMetadata = ReadableVMetadata<BankFileMeta>;
+pub type WritableBankMetadata<'a> = WritableVMetadata<'a, BankFileMeta, BankArchive>;
 pub type BankDirectory<'a> = VDirectory<'a, BankFileMeta, BankArchive>;
 
-pub struct BankArchive { entries: HashMap<VPath, BankFile>, properties: HashMap<String, String> }
 
+pub struct BankArchive {
+    file_system: MemoryFileSystem<BankFileMeta>,
+    properties: HashMap<String, String>
+}
 
+impl Deref for BankArchive {
+    type Target = MemoryFileSystem<BankFileMeta>;
+
+    fn deref(&self) -> &Self::Target { &self.file_system }
+}
+
+impl DerefMut for BankArchive {
+    fn deref_mut(&mut self) -> &mut Self::Target  { &mut self.file_system }
+}
+
+#[allow(dead_code)]
 impl BankArchive {
-    pub fn header_size(&self) -> usize { self.metadata_size() + self.version_size() }
+    pub fn header_size(&self) -> rfsa::Result<usize> { Ok(self.metadata_size()? + self.version_size()) }
 
-    pub fn metadata_size(&self) -> usize { self.entries.keys().map(|path|
+    pub fn metadata_size(&self) -> rfsa::Result<usize> { Ok(self.fs_iter()?.map(|path|
         path.as_bytes().len() + 21
-    ).sum() }
+    ).sum()) }
 
     pub fn version_size(&self) -> usize { 22 + self.properties.iter().map(|(k, v) |
         k.as_bytes().len() + v.as_bytes().len() + 2
     ).sum::<usize>() }
 
-    pub fn property_get(&self, name: &str) -> Option<&String> {
-        self.properties.get(name)
-    }
+    pub fn property_get(&self, name: &str) -> Option<&String> { self.properties.get(name) }
 
     pub fn property_insert(&mut self, name: &str, value: &str) -> Option<String> {
         self.properties.insert(name.to_string(), value.to_string())
     }
 
-}
-
-impl VFileSystem<BankFileMeta> for BankArchive {
-    fn paths(&self) -> VFSResult<VPathIterator> {
-        Ok(Box::new(self.entries.keys().cloned().collect::<Vec<VPath>>().into_iter()))
+    pub fn create(root: VPath, entries: HashMap<VPath, BankFile>, properties: HashMap<String, String>) -> Self {
+        Self { file_system: MemoryFileSystem::new(root, entries), properties, }
     }
 
-    fn path_iter(&self, path_prefix: String, recursive: bool) -> VFSResult<VPathIterator> {
-        let prefix_len = path_prefix.len();
-        Ok(Box::new(self.entries.keys().filter( |candidate| {
-            candidate.starts_with(path_prefix.as_str()) && (!recursive || !candidate[prefix_len..].contains(GFS_SEPARATOR))
-        }).cloned().collect::<Vec<VPath>>().into_iter()))
-    }
-}
-
-
-impl VFileContainer<BankFileMeta, BankArchive> for BankArchive {
-    fn file_remove(&mut self, path: &VPath) -> VFSResult<VFile<BankFileMeta>> {
-        self.entries.remove(path).ok_or(VFSError::EntryNotFound)
-    }
-
-    fn file_exists(&self, path: &VPath) -> VFSResult<bool> {
-        Ok(self.entries.contains_key(path))
-    }
-
-    fn file_insert(&mut self, path: &VPath, file: VFile<BankFileMeta>) -> VFSResult<Option<BankFile>> {
-        Ok(self.entries.insert(path.clone(), file))
-    }
-
-    fn file_mut(&mut self, path: &VPath) -> VFSResult<&mut VFile<BankFileMeta>> {
-        self.entries.get_mut(path).ok_or(VFSError::EntryNotFound)
-    }
-
-    fn file_get(&self, path: &VPath) -> VFSResult<&VFile<BankFileMeta>> {
-        self.entries.get(path).ok_or(VFSError::EntryNotFound)
-    }
-
-    fn dir_exists(&self, path: &VPath) -> VFSResult<bool> {
-        Ok(self.entries.keys().find(|p| {
-            p.starts_with(path.as_directory_string().as_str())
-        }) != None)
-    }
 }
 
 
--- src/io/read.rs
+++ src/io/read.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
-use std::io::{Read, Seek};
+use std::io::{Read, Seek, SeekFrom};
+use std::sync::Arc;
 use byteorder::{LittleEndian, ReadBytesExt};
-use crate::{BankFileMeta, BankMime, BankReadOptions};
+use rfsa::{PathLike, VPath};
+use crate::{BankArchive, BankFile, BankFileMeta, BankMime, BankReadOptions};
 use crate::error::{BankError, BankResult, MissingTerminatorType};
-
-
 
 trait PboReadExt: Read + Sized {
     fn read_bank_string(&mut self, require_terminator: bool) -> BankResult<String> {
@@ -57,66 +57,68 @@ impl<T: Read + Seek> PboReadExt for T  {
 
 }
 
-// impl BankArchive {
-//     fn try_read<T: Read + Seek>(reader: &mut T, filename: &str, options: &BankReadOptions) -> BankResult<BankArchive> {
-//         let (entries, properties, root) = {
-//             let mut entry_index: i32 = 0;
-//             let mut start_offset: i32 = 0;
-//             let mut properties: HashMap<String, String> = HashMap::new();
-//             let mut metas: HashMap<String, BankFileMeta> = HashMap::new();
-//             let mut encountered_version: bool = false;
-//             loop {
-//                 let (name, mut meta) = reader.read_bank_entry_meta(&options)?;
-//                 if meta.mime == BankMime::Version {
-//                     if entry_index == 0 && meta.mime == BankMime::Version && name.len() == 0 {
-//                         reader.read_bank_properties(&mut properties, options)?;
-//                         encountered_version = true;
-//                     } else if !options.allow_post_mature_version {
-//                         return Err(BankError::PostMatureVersion);
-//                     }
-//                 }
-//
-//                 if entry_index == 0 && !encountered_version && options.requires_version_header() {
-//                     return Err(BankError::MissingVersionEntry);
-//                 }
-//
-//                 if meta.buffer_offset == 0 && meta.mime == BankMime::Decompressed && name.len() == 0 &&
-//                     meta.buffer_length == 0 && meta.timestamp == 0 && meta.content_length == 0 {
-//                     break;
-//                 }
-//
-//                 meta.buffer_offset = start_offset as u32 as u64;
-//                 let entry_length = meta.buffer_length as i32;
-//                 if !options.respect_signedness_bug && entry_length < 0 {
-//                     return Err(BankError::InvalidDataOffset);
-//                 }
-//                 start_offset += entry_length;
-//
-//                 entry_index += 1;
-//                 if meta.content_length >= options.valid_entry_threshold {
-//                     metas.insert(name, meta);
-//                 }
-//             }
-//             let root = match properties.get("prefix") {
-//                 None => BankArchive::normalize_path(filename),
-//                 Some(p) => BankArchive::normalize_path(p)
-//             };
-//             let buffer_start = reader.stream_position()?;
-//
-//             // let entries: HashMap<GfsPath, BankFile> = metas.drain().map(|(mut name, mut entry)| {
-//             //     entry.buffer_offset += buffer_start;
-//             //     reader.seek(SeekFrom::Start(entry.buffer_offset)).unwrap();
-//             //     let mut data: Vec<u8> = vec![0; entry.buffer_length as usize];
-//             //     reader.read_exact(&mut *data).unwrap();
-//             //     return (BankArchive::normalize_path(&*name), BankFile::create(entry, Arc::from(data.as_slice())));
-//             // }).collect();
-//
-//             (metas, properties, root)
-//         };
-//         todo!();
-//     }
-//
-//     fn read<T: Read + Seek>(reader: &mut T, filename: &str, options: &BankReadOptions) -> BankArchive {
-//         Self::try_read(reader, filename, options).unwrap()
-//     }
-// }
+#[allow(dead_code)]
+impl BankArchive {
+    fn try_read<T: Read + Seek>(reader: &mut T, filename: &str, options: &BankReadOptions) -> BankResult<BankArchive> {
+        let (root, entries, properties) = {
+            let mut entry_index: i32 = 0;
+            let mut start_offset: i32 = 0;
+            let mut properties: HashMap<String, String> = HashMap::new();
+            let mut metas: HashMap<String, BankFileMeta> = HashMap::new();
+            let mut encountered_version: bool = false;
+            loop {
+                let (name, mut meta) = reader.read_bank_entry_meta(&options)?;
+                if meta.mime == BankMime::Version {
+                    if entry_index == 0 && meta.mime == BankMime::Version && name.len() == 0 {
+                        reader.read_bank_properties(&mut properties, options)?;
+                        encountered_version = true;
+                    } else if !options.allow_post_mature_version {
+                        return Err(BankError::PostMatureVersion);
+                    }
+                }
+
+                if entry_index == 0 && !encountered_version && options.requires_version_header() {
+                    return Err(BankError::MissingVersionEntry);
+                }
+
+                if meta.buffer_offset == 0 && meta.mime == BankMime::Decompressed && name.len() == 0 &&
+                    meta.buffer_length == 0 && meta.timestamp == 0 && meta.content_length == 0 {
+                    break;
+                }
+
+                meta.buffer_offset = start_offset as u32 as u64;
+                let entry_length = meta.buffer_length as i32;
+                if !options.respect_signedness_bug && entry_length < 0 {
+                    return Err(BankError::InvalidDataOffset);
+                }
+                start_offset += entry_length;
+
+                entry_index += 1;
+                if meta.content_length >= options.valid_entry_threshold {
+                    metas.insert(name, meta);
+                }
+            }
+            let root = match properties.get("prefix") {
+                None => VPath::normalized(filename),
+                Some(p) => VPath::normalized(&*p)
+            };
+            let buffer_start = reader.stream_position()?;
+
+            let entries: HashMap<VPath, BankFile> = metas.drain().map(|(name, mut entry)| {
+                entry.buffer_offset += buffer_start;
+                reader.seek(SeekFrom::Start(entry.buffer_offset)).unwrap();
+                let mut data: Vec<u8> = vec![0; entry.buffer_length as usize];
+                reader.read_exact(&mut *data).unwrap();
+                return (VPath::normalized(&*name), BankFile::create(entry, Arc::from(data.as_slice())));
+            }).collect();
+
+            (root, entries, properties)
+        };
+        Ok(BankArchive::create(root, entries, properties))
+    }
+
+
+    fn read<T: Read + Seek>(reader: &mut T, filename: &str, options: &BankReadOptions) -> BankArchive {
+        Self::try_read(reader, filename, options).unwrap()
+    }
+}
